2b5d1215cb16c584dc4471546fb16416ced6ec70
Widad Alkibsi <widad.alk@gmail.com>
2020-07-17 17:51:50 +0300

    changed jwt (expired)

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -23,14 +23,6 @@ class AuthError(Exception):
 
 ## Auth Header
 
-'''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
 # def get_token_auth_header():
 #    raise Exception('Not Implemented')
 
@@ -59,18 +51,6 @@ def get_token_auth_header():
 
     return header_parts[1]
 
-
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
-
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
 
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
